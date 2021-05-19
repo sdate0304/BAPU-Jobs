4dcdb0d155b4df0881cad557b4a0aad19f45e1b5
Clemenson, Sara <SClemenson@wrberkley.com>
2021-05-19 21:45:47 +0000

    BAPU-483: update sql agent job script after adding step to call bapu_core.dbo.p_rpt_premium_band_package_table_prep
    git-tfs-id: [http://btstfs:8080/tfs/BAPUCollection/]$/BAPU/Prod/SQL Agent Jobs;C1028

--- BAPU (5) - Daily - Post-Core Processing.sql
+++ BAPU (5) - Daily - Post-Core Processing.sql
@@ -1,11 +1,11 @@
 USE [msdb]
 GO
 
-/****** Object:  Job [BAPU (5) - Daily - Post-Core Processing]    Script Date: 03/17/2021 4:06:58 PM ******/
+/****** Object:  Job [BAPU (5) - Daily - Post-Core Processing]    Script Date: 05/19/2021 4:44:45 PM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 03/17/2021 4:06:58 PM ******/
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 05/19/2021 4:44:46 PM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
@@ -26,7 +26,7 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'BAPU (5) - Daily - Post-Core
 		@owner_login_name=N'WRBTS\SVC-BAP-P-DW', 
 		@notify_email_operator_name=N'BAPU DW Notifications', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Run p_Policy_Transaction_Earnings (Start/Complete Batch_Type_Key 21)]    Script Date: 03/17/2021 4:06:59 PM ******/
+/****** Object:  Step [Run p_Policy_Transaction_Earnings (Start/Complete Batch_Type_Key 21)]    Script Date: 05/19/2021 4:44:47 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Run p_Policy_Transaction_Earnings (Start/Complete Batch_Type_Key 21)', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
@@ -77,7 +77,7 @@ IF @batch_status_out = ''Running''
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Start After Core Batch (Batch_Type_Key = 22)]    Script Date: 03/17/2021 4:06:59 PM ******/
+/****** Object:  Step [Start After Core Batch (Batch_Type_Key = 22)]    Script Date: 05/19/2021 4:44:47 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Start After Core Batch (Batch_Type_Key = 22)', 
 		@step_id=2, 
 		@cmdexec_success_code=0, 
@@ -100,7 +100,7 @@ EXEC BAPU_Logging.Batch.p_Batch_Log
 		@database_name=N'BAPU_Logging', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Load Claimtrak Interface Table (p_Build_BAPU_Claimtrak_Data)]    Script Date: 03/17/2021 4:06:59 PM ******/
+/****** Object:  Step [[     Load Claimtrak Interface Table (p_Build_BAPU_Claimtrak_Data)]    Script Date: 05/19/2021 4:44:48 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Load Claimtrak Interface Table (p_Build_BAPU_Claimtrak_Data)', 
 		@step_id=3, 
 		@cmdexec_success_code=0, 
@@ -116,7 +116,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Lo
 		@database_name=N'BAPU', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Load 5-Year History Tables (p_dm_Five_Year_History)]    Script Date: 03/17/2021 4:06:59 PM ******/
+/****** Object:  Step [[     Load 5-Year History Tables (p_dm_Five_Year_History)]    Script Date: 05/19/2021 4:44:48 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Load 5-Year History Tables (p_dm_Five_Year_History)', 
 		@step_id=4, 
 		@cmdexec_success_code=0, 
@@ -131,7 +131,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Lo
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     London Rate Monitoring (p_rpt_London_Rate_Monitor)]    Script Date: 03/17/2021 4:06:59 PM ******/
+/****** Object:  Step [[     London Rate Monitoring (p_rpt_London_Rate_Monitor)]    Script Date: 05/19/2021 4:44:48 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     London Rate Monitoring (p_rpt_London_Rate_Monitor)', 
 		@step_id=5, 
 		@cmdexec_success_code=0, 
@@ -146,7 +146,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Lo
 		@database_name=N'BAPU', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Load Premium vs. Budget Tables (p_rpt_Premium_vs_Budget_Populate_Table)]    Script Date: 03/17/2021 4:07:00 PM ******/
+/****** Object:  Step [[     Load Premium vs. Budget Tables (p_rpt_Premium_vs_Budget_Populate_Table)]    Script Date: 05/19/2021 4:44:48 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Load Premium vs. Budget Tables (p_rpt_Premium_vs_Budget_Populate_Table)', 
 		@step_id=6, 
 		@cmdexec_success_code=0, 
@@ -161,7 +161,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Lo
 		@database_name=N'BAPU', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Load Run Rate Tables (p_Run_Rate_Populate_Table)]    Script Date: 03/17/2021 4:07:00 PM ******/
+/****** Object:  Step [[     Load Run Rate Tables (p_Run_Rate_Populate_Table)]    Script Date: 05/19/2021 4:44:48 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Load Run Rate Tables (p_Run_Rate_Populate_Table)', 
 		@step_id=7, 
 		@cmdexec_success_code=0, 
@@ -176,7 +176,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Lo
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Billing Account Tables (p_rpt_Billing_Account_Threshold_Populate_Tables)]    Script Date: 03/17/2021 4:07:00 PM ******/
+/****** Object:  Step [[     Billing Account Tables (p_rpt_Billing_Account_Threshold_Populate_Tables)]    Script Date: 05/19/2021 4:44:49 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Billing Account Tables (p_rpt_Billing_Account_Threshold_Populate_Tables)', 
 		@step_id=8, 
 		@cmdexec_success_code=0, 
@@ -191,7 +191,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Bi
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     OWSY Partner Report Populate Data (p_dm_OWSY_Partner_Data)]    Script Date: 03/17/2021 4:07:00 PM ******/
+/****** Object:  Step [[     OWSY Partner Report Populate Data (p_dm_OWSY_Partner_Data)]    Script Date: 05/19/2021 4:44:49 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     OWSY Partner Report Populate Data (p_dm_OWSY_Partner_Data)', 
 		@step_id=9, 
 		@cmdexec_success_code=0, 
@@ -206,7 +206,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     OW
 		@database_name=N'BAPU', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Populate Agent Portal Tables in BAP_Agent_Portal Database (p_Load_BAP_Agent_Portal_Tables)]    Script Date: 03/17/2021 4:07:00 PM ******/
+/****** Object:  Step [[     Populate Agent Portal Tables in BAP_Agent_Portal Database (p_Load_BAP_Agent_Portal_Tables)]    Script Date: 05/19/2021 4:44:49 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Populate Agent Portal Tables in BAP_Agent_Portal Database (p_Load_BAP_Agent_Portal_Tables)', 
 		@step_id=10, 
 		@cmdexec_success_code=0, 
@@ -221,7 +221,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Po
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Populate Risk Clearance Table (p_rpt_Risk_Clearance_Populate_Table)]    Script Date: 03/17/2021 4:07:00 PM ******/
+/****** Object:  Step [[     Populate Risk Clearance Table (p_rpt_Risk_Clearance_Populate_Table)]    Script Date: 05/19/2021 4:44:49 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Populate Risk Clearance Table (p_rpt_Risk_Clearance_Populate_Table)', 
 		@step_id=11, 
 		@cmdexec_success_code=0, 
@@ -236,7 +236,7 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Po
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Populate Limit Band Report Tables (p_rpt_Limit_Band_Table_Prep)]    Script Date: 03/17/2021 4:07:01 PM ******/
+/****** Object:  Step [[     Populate Limit Band Report Tables (p_rpt_Limit_Band_Table_Prep)]    Script Date: 05/19/2021 4:44:49 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Populate Limit Band Report Tables (p_rpt_Limit_Band_Table_Prep)', 
 		@step_id=12, 
 		@cmdexec_success_code=0, 
@@ -251,9 +251,24 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Po
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Populate Table for BPMi Auto Renew Genesys Data (p_BPMi_Automated_Renewal_Sent_Populate_Table)]    Script Date: 03/17/2021 4:07:01 PM ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Populate Table for BPMi Auto Renew Genesys Data (p_BPMi_Automated_Renewal_Sent_Populate_Table)', 
+/****** Object:  Step [[     Populate Premium Band Report Tables (p_rpt_Premium_Band_Package_Table_Prep)]    Script Date: 05/19/2021 4:44:50 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Populate Premium Band Report Tables (p_rpt_Premium_Band_Package_Table_Prep)', 
 		@step_id=13, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=3, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'EXEC dbo.p_rpt_Premium_Band_Package_Table_Prep;', 
+		@database_name=N'BAPU_CORE', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [[     Populate Table for BPMi Auto Renew Genesys Data (p_BPMi_Automated_Renewal_Sent_Populate_Table)]    Script Date: 05/19/2021 4:44:50 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Populate Table for BPMi Auto Renew Genesys Data (p_BPMi_Automated_Renewal_Sent_Populate_Table)', 
+		@step_id=14, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -266,9 +281,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Po
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Load Transaction + Detail Data Mart Table (p_dm_Transaction_Level_Plus_Detail)]    Script Date: 03/17/2021 4:07:01 PM ******/
+/****** Object:  Step [[     Load Transaction + Detail Data Mart Table (p_dm_Transaction_Level_Plus_Detail)]    Script Date: 05/19/2021 4:44:50 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Load Transaction + Detail Data Mart Table (p_dm_Transaction_Level_Plus_Detail)', 
-		@step_id=14, 
+		@step_id=15, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -281,9 +296,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Lo
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Populate xref_Current_Coverage_State Table]    Script Date: 03/17/2021 4:07:01 PM ******/
+/****** Object:  Step [[     Populate xref_Current_Coverage_State Table]    Script Date: 05/19/2021 4:44:50 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Populate xref_Current_Coverage_State Table', 
-		@step_id=15, 
+		@step_id=16, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -296,9 +311,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Po
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [[     Populate xref_Current_Coverage_ASLOB Table]    Script Date: 03/17/2021 4:07:01 PM ******/
+/****** Object:  Step [[     Populate xref_Current_Coverage_ASLOB Table]    Script Date: 05/19/2021 4:44:50 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Populate xref_Current_Coverage_ASLOB Table', 
-		@step_id=16, 
+		@step_id=17, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -311,9 +326,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'[     Po
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Complete After Core Batch Processing (Batch_Type_Key = 22)]    Script Date: 03/17/2021 4:07:01 PM ******/
+/****** Object:  Step [Complete After Core Batch Processing (Batch_Type_Key = 22)]    Script Date: 05/19/2021 4:44:51 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Complete After Core Batch Processing (Batch_Type_Key = 22)', 
-		@step_id=17, 
+		@step_id=18, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -350,9 +365,9 @@ IF @batch_status_out = ''Running''
 		@database_name=N'BAPU_Logging', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Rating Data]    Script Date: 03/17/2021 4:07:01 PM ******/
+/****** Object:  Step [Rating Data]    Script Date: 05/19/2021 4:44:51 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Rating Data', 
-		@step_id=18, 
+		@step_id=19, 
 		@cmdexec_success_code=0, 
 		@on_success_action=1, 
 		@on_success_step_id=0, 
