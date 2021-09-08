8ffb2a0c5902091945a9c6c394bdb8f86ca6c5ff
Clemenson, Sara <SClemenson@wrberkley.com>
2021-09-08 15:16:23 +0000

    BAPU-524: checking in updated sql agent job scripts for dev and prod after correcting the IF logic to be >= 1 instead of >1 to run the clean up script.
    git-tfs-id: [http://btstfs:8080/tfs/BAPUCollection/]$/BAPU/Prod/SQL Agent Jobs;C1193

--- BAPU (2) - Daily - Pre-Core Processing.sql
+++ BAPU (2) - Daily - Pre-Core Processing.sql
@@ -1,11 +1,11 @@
 USE [msdb]
 GO
 
-/****** Object:  Job [BAPU (2) - Daily - Pre-Core Processing]    Script Date: 03/17/2021 4:05:36 PM ******/
+/****** Object:  Job [BAPU (2) - Daily - Pre-Core Processing]    Script Date: 09/08/2021 10:08:30 AM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 03/17/2021 4:05:36 PM ******/
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 09/08/2021 10:08:30 AM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
@@ -26,9 +26,91 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'BAPU (2) - Daily - Pre-Core 
 		@owner_login_name=N'WRBTS\SVC-BAP-P-DW', 
 		@notify_email_operator_name=N'BAPU DW Notifications', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Start Pre-Core Data Work Batch (Batch_Type_Key = 18)]    Script Date: 03/17/2021 4:05:37 PM ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Start Pre-Core Data Work Batch (Batch_Type_Key = 18)', 
+/****** Object:  Step [TEMPORARY BUS Custom Property Clean Up]    Script Date: 09/08/2021 10:08:31 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'TEMPORARY BUS Custom Property Clean Up', 
 		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=3, 
+		@on_success_step_id=0, 
+		@on_fail_action=3, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'---- This step is being added temporarily to account for a BUS custom property duplication issue
+---- For additional information please see: https://jira.wrberkley.com/browse/BAPU-524
+USE BAPU_PolicyAdministration;
+
+DROP TABLE IF EXISTS #DupCustomProperty;
+DROP TABLE IF EXISTS #CustPropToDelete;
+
+DECLARE @RowCount INT = 0;
+
+---- Are there any policies duplicated in the BUS policy view?
+SELECT  @RowCount = SUM(row_count.pol_count)
+FROM    ( SELECT    COUNT(DISTINCT vcbpb.policy_source_key) AS pol_count
+          FROM  BAPU_CORE.dbo.v_core_bld_policy_BUS AS vcbpb
+          GROUP BY vcbpb.policy_source_key
+          HAVING   COUNT(*) > 1 ) AS row_count;
+
+--SELECT  @RowCount;
+
+---- If there are duplicated policies then find the affected policies and their max & min create date
+IF @RowCount >= 1
+    BEGIN
+
+        ---- Identify Policies on the BUS side that have more than 12 custom property IDs associated with the Policy (each BUS policy should ONLY have 12 as of the time this query was written)
+        ---- Queries above can be used for additional research
+        SELECT  cp.InstanceId AS PolicyId
+               ,p.PolicyNumber
+               ,COUNT(cp.CustomPropertyId) AS CustomPropertyCount ---- Should be 12 or less
+               ,MIN(cp.CreateDate) AS MinCreateDate
+               ,MAX(cp.CreateDate) AS MaxCreateDate
+        INTO    #DupCustomProperty
+        FROM    BAPU_PolicyAdministration.Global.CustomProperty AS cp
+        INNER JOIN BAPU_PolicyAdministration.Global.CustomPropertyDefinition AS cpd ON cpd.CustomPropertyDefinitionId = cp.CustomPropertyDefinitionId
+        LEFT OUTER JOIN BAPU_PolicyAdministration.Global.EntityType AS et ON et.EntityTypeId = cp.EntityTypeId
+        LEFT OUTER JOIN BAPU_PolicyAdministration.PolicyAdministration.Policy AS p ON p.PolicyId = cp.InstanceId
+        WHERE   cp.EntityTypeId = 23 ---- policy
+        GROUP BY cp.InstanceId
+                ,p.PolicyNumber
+        HAVING  COUNT(cp.CustomPropertyId) > 12
+        ORDER BY cp.InstanceId
+                ,p.PolicyNumber;
+
+        ---- Here are the custom property ids to DELETE (those that match the maximum create date
+        SELECT  DISTINCT cp.CustomPropertyId
+        INTO    #CustPropToDelete
+        FROM    BAPU_PolicyAdministration.Global.CustomProperty AS cp
+        INNER JOIN BAPU_PolicyAdministration.Global.CustomPropertyDefinition AS cpd ON cpd.CustomPropertyDefinitionId = cp.CustomPropertyDefinitionId
+        LEFT OUTER JOIN BAPU_PolicyAdministration.Global.EntityType AS et ON et.EntityTypeId = cp.EntityTypeId
+        LEFT OUTER JOIN BAPU_PolicyAdministration.PolicyAdministration.Policy AS p ON p.PolicyId = cp.InstanceId
+        INNER JOIN #DupCustomProperty AS dcp ON dcp.PolicyId = cp.InstanceId
+                                            AND TRY_CAST(dcp.MaxCreateDate AS DATE) = TRY_CAST(cp.CreateDate AS DATE)
+        WHERE   cp.EntityTypeId = 23 ---- policy
+        ;
+
+        ---- Delete those custom properties
+        DELETE
+        --SELECT  *
+        FROM BAPU_PolicyAdministration.Global.CustomProperty
+        WHERE   EXISTS ( SELECT cp.CustomPropertyId
+                         FROM   #CustPropToDelete AS cp
+                         WHERE  cp.CustomPropertyId = BAPU_PolicyAdministration.Global.CustomProperty.CustomPropertyId );
+    END;
+ELSE
+    BEGIN
+        PRINT ''No Duplicated Policies'';
+    END;
+
+DROP TABLE IF EXISTS #DupCustomProperty;
+DROP TABLE IF EXISTS #CustPropToDelete;', 
+		@database_name=N'BAPU_PolicyAdministration', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Start Pre-Core Data Work Batch (Batch_Type_Key = 18)]    Script Date: 09/08/2021 10:08:31 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Start Pre-Core Data Work Batch (Batch_Type_Key = 18)', 
+		@step_id=2, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -49,9 +131,9 @@ EXEC BAPU_Logging.Batch.p_Batch_Log
 		@database_name=N'BAPU_Logging', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [p_PDR_SSP_Insert_LastRiskUnitVersion]    Script Date: 03/17/2021 4:05:37 PM ******/
+/****** Object:  Step [p_PDR_SSP_Insert_LastRiskUnitVersion]    Script Date: 09/08/2021 10:08:31 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'p_PDR_SSP_Insert_LastRiskUnitVersion', 
-		@step_id=2, 
+		@step_id=3, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -64,9 +146,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'p_PDR_SS
 		@database_name=N'BAPU', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [p_PDR_SSP_Insert_LastCoverageUnitVersion]    Script Date: 03/17/2021 4:05:37 PM ******/
+/****** Object:  Step [p_PDR_SSP_Insert_LastCoverageUnitVersion]    Script Date: 09/08/2021 10:08:31 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'p_PDR_SSP_Insert_LastCoverageUnitVersion', 
-		@step_id=3, 
+		@step_id=4, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -79,9 +161,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'p_PDR_SS
 		@database_name=N'BAPU', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [p_BUS_Custom_Property_Risk_Detail_Load]    Script Date: 03/17/2021 4:05:38 PM ******/
+/****** Object:  Step [p_BUS_Custom_Property_Risk_Detail_Load]    Script Date: 09/08/2021 10:08:31 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'p_BUS_Custom_Property_Risk_Detail_Load', 
-		@step_id=4, 
+		@step_id=5, 
 		@cmdexec_success_code=0, 
 		@on_success_action=3, 
 		@on_success_step_id=0, 
@@ -94,9 +176,9 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'p_BUS_Cu
 		@database_name=N'BAPU_CORE', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Complete Pre-Core Data Work Batch (Batch_Type_Key = 18)]    Script Date: 03/17/2021 4:05:38 PM ******/
+/****** Object:  Step [Complete Pre-Core Data Work Batch (Batch_Type_Key = 18)]    Script Date: 09/08/2021 10:08:31 AM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Complete Pre-Core Data Work Batch (Batch_Type_Key = 18)', 
-		@step_id=5, 
+		@step_id=6, 
 		@cmdexec_success_code=0, 
 		@on_success_action=1, 
 		@on_success_step_id=0, 
